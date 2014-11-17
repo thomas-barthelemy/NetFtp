f41079ac97debcc411df0aa7d06caeef6acfbf02
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-11-17 18:52:00 +0800

    XML Doc for FtpDownloadProgressChangedEventArgs

--- NetFtp/NetFtpEventArgs/FtpDownloadProgressChangedEventArgs.cs
+++ NetFtp/NetFtpEventArgs/FtpDownloadProgressChangedEventArgs.cs
@@ -3,8 +3,22 @@ using NetFtp.Utils;
 
 namespace NetFtp.NetFtpEventArgs
 {
+    /// <summary>
+    ///     Provides data for the FtpDownloadProgressChanged event
+    /// </summary>
     public class FtpDownloadProgressChangedEventArgs : EventArgs
     {
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="FtpDownloadProgressChangedEventArgs" />
+        ///     with the specified state and total number of bytes received.
+        /// </summary>
+        /// <param name="bytesReceived">
+        ///     The number of bytes received so far during the transaction.
+        /// </param>
+        /// <param name="totalBytes">
+        ///     The total number of bytes of the file being downloaded.
+        /// </param>
         public FtpDownloadProgressChangedEventArgs(long bytesReceived,
             long totalBytes)
         {
@@ -12,10 +26,19 @@ namespace NetFtp.NetFtpEventArgs
             TotalBytes = totalBytes;
         }
 
+        /// <summary>
+        ///     Gets the number of bytes received so far during the transaction.
+        /// </summary>
         public long BytesReceived { get; private set; }
 
+        /// <summary>
+        ///     Gets the total number of bytes of the file being downloaded.
+        /// </summary>
         public long TotalBytes { get; private set; }
 
+        /// <summary>
+        ///     Gets the download progress as a percentage.
+        /// </summary>
         public int Percent
         {
             get { return MathUtil.GetCompletionPercentage(BytesReceived, TotalBytes); }
--- NetFtp/NetFtpEventArgs/FtpFileExistsCompletedEventArgs.cs
+++ NetFtp/NetFtpEventArgs/FtpFileExistsCompletedEventArgs.cs
@@ -1,13 +1,35 @@
-﻿using System;
+﻿using System.Net;
 using NetFtp.Utils;
 
 namespace NetFtp.NetFtpEventArgs
 {
+    /// <summary>
+    ///     Provides data for the FtpFileExistsCompleted event.
+    /// </summary>
     public class FtpFileExistsCompletedEventArgs
     {
-        public bool FileExists { get; set; }
-        public Exception Exception { get; set; }
-        public long RemotefileSize { get; set; }
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="FtpFileExistsCompletedEventArgs" />
+        ///     with the specified state and total number of bytes received.
+        /// </summary>
+        /// <param name="fileExists"></param>
+        /// <param name="remoteFileSize"></param>
+        public FtpFileExistsCompletedEventArgs(bool fileExists, long remoteFileSize)
+        {
+            FileExists = fileExists;
+            RemotefileSize = remoteFileSize;
+
+        }
+
+        public FtpFileExistsCompletedEventArgs(WebException ex)
+        {
+            Exception = ex;
+        }
+
+        public bool FileExists { get; private set; }
+        public WebException Exception { get; private set; }
+        public long RemotefileSize { get; private set; }
 
         public TransmissionState State
         {
