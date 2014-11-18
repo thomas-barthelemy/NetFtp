9410760a7ec6f2b159d6e99b605dbad08664b571
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-11-18 13:58:59 +0800

    XML Doc for Utils/*

--- NetFtp/Utils/TransmissionState.cs
+++ NetFtp/Utils/TransmissionState.cs
@@ -1,13 +1,38 @@
 ﻿namespace NetFtp.Utils
 {
-    public enum TransmissionState
+    /// <summary>
+    ///     Represents the state of a transaction.
+    /// </summary>
+    public enum TransactionState
     {
+        /// <summary>
+        ///     The transaction terminated unexpectedly. 
+        /// </summary>
         Failed,
+        /// <summary>
+        ///     The transaction terminated successfully.
+        /// </summary>
         Success,
-        LocalFileBiggerAsRemoteFile,
+        /// <summary>
+        ///     The download resume failed as the local file is bigger than
+        ///     the file to download.
+        /// </summary>
+        LocalFileBiggerThanRemoteFile,
+        /// <summary>
+        ///     The FTP Client is checking if the specified remote directory exists.
+        /// </summary>
         ProofingDirExits,
+        /// <summary>
+        ///     The FTP Client is creating a necessary remote directory.
+        /// </summary>
         CreatingDir,
+        /// <summary>
+        ///     The upload transaction is active and in progress.
+        /// </summary>
         Uploading,
+        /// <summary>
+        ///     The transaction terminated due to an abort request.
+        /// </summary>
         Aborted
     }
 }
