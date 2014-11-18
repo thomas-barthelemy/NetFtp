525cfd3660050ce794c7f6c35d97eae8e1518935
Thomas Barthelemy <thomasb@gobeta.com.cn>
2014-11-18 13:27:51 +0800

    XML Doc for FtpUploadProgressChangedEventArgs

--- NetFtp/NetFtpEventArgs/FtpUploadProgressChangedEventArgs.cs
+++ NetFtp/NetFtpEventArgs/FtpUploadProgressChangedEventArgs.cs
@@ -3,8 +3,24 @@ using NetFtp.Utils;
 
 namespace NetFtp.NetFtpEventArgs
 {
+    /// <summary>
+    ///     Provides data for the FtpUploadProgressChanged event.
+    /// </summary>
     public class FtpUploadProgressChangedEventArgs : EventArgs
     {
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="FtpUploadProgressChangedEventArgs" />
+        ///     with the specified number of bytes sent,
+        ///     total number of bytes to send
+        ///     and sets the Transmission State to "Uploading".
+        /// </summary>
+        /// <param name="bytesSent">
+        ///     The number of bytes sent so far during the transaction.
+        /// </param>
+        /// <param name="totalBytes">
+        ///     The total number of bytes to send.
+        /// </param>
         public FtpUploadProgressChangedEventArgs(long bytesSent, long totalBytes)
         {
             TransmissionState = TransmissionState.Uploading;
@@ -12,17 +28,30 @@ namespace NetFtp.NetFtpEventArgs
             TotalBytes = totalBytes;
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="FtpUploadProgressChangedEventArgs" />
+        ///     with the specified transmission State.
+        /// </summary>
+        /// <param name="transmissionState"></param>
         public FtpUploadProgressChangedEventArgs(TransmissionState transmissionState)
         {
             TransmissionState = transmissionState;
-            BytesSent = 0L;
-            TotalBytes = 0L;
         }
 
+        /// <summary>
+        ///     Gets the number of bytes sent so far during the transaction.
+        /// </summary>
         public long BytesSent { get; private set; }
 
+        /// <summary>
+        ///     Gets the total number of bytes to send (local file size).
+        /// </summary>
         public long TotalBytes { get; private set; }
 
+        /// <summary>
+        ///     Gets the progress of the transaction as a percentage.
+        /// </summary>
         public int Percentage
         {
             get
@@ -31,6 +60,9 @@ namespace NetFtp.NetFtpEventArgs
             }
         }
 
+        /// <summary>
+        ///     Gets the state of the transaction
+        /// </summary>
         public TransmissionState TransmissionState { get; private set; }
     }
 }
